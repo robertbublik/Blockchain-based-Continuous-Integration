26f5756ed8a93ce3af484a38a746f16a086251ab
Robert Bublik <pulzlulz@gmail.com>
2021-05-10 20:29:35 +0200

    add BCI mining functions

--- node/http_routes.go
+++ node/http_routes.go
@@ -12,26 +12,35 @@ type ErrRes struct {
 }
 
 type BalancesRes struct {
-	Hash     database.Hash             `json:"block_hash"`
+	Hash     database.Hash             `json:"blockHash"`
 	Balances map[database.Account]uint `json:"balances"`
 }
 
-type TxAddReq struct {
-	From  string `json:"from"`
-	To    string `json:"to"`
-	Value uint   `json:"value"`
-	Data  string `json:"data"`
+type TxReq struct {
+	From  		Account 	`json:"from"`
+	Value 		uint    	`json:"value"`
+	Repository  string  	`json:"repository"`
+	Commit 		20[byte] 	`json:"commit"`
+	prevCommit 	20[byte] 	`json:"prevCommit"`
+	Time  		uint64  	`json:"time"`
+	Mining		bool		`json:"mining"`
 }
 
 type TxAddRes struct {
 	Success bool `json:"success"`
 }
 
+type TxMineRes struct {
+	Success bool `json:"success"`
+}
+
 type StatusRes struct {
-	Hash       database.Hash       `json:"block_hash"`
-	Number     uint64              `json:"block_number"`
-	KnownPeers map[string]PeerNode `json:"peers_known"`
-	PendingTXs []database.Tx       `json:"pending_txs"`
+	Hash       	database.Hash       `json:"blockHash"`
+	BlockIndex 	uint64              `json:"blockIndex"`
+	TxIndex 	uint64              `json:"txIndex"`
+	KnownPeers 	map[string]PeerNode `json:"peersKnown"`
+	PendingTXs 	[]database.Tx       `json:"pendingTxs"`
+	MiningTXs  	[]database.Tx       `json:"miningTxs"`
 }
 
 type SyncRes struct {
@@ -47,16 +56,36 @@ func listBalancesHandler(w http.ResponseWriter, r *http.Request, state *database
 	writeRes(w, BalancesRes{state.LatestBlockHash(), state.Balances})
 }
 
+// adds transaction to BCI
 func txAddHandler(w http.ResponseWriter, r *http.Request, node *Node) {
-	req := TxAddReq{}
+	req := TxReq{}
 	err := readReq(r, &req)
 	if err != nil {
 		writeErrRes(w, err)
 		return
 	}
 
-	tx := database.NewTx(database.NewAccount(req.From), database.NewAccount(req.To), req.Value, req.Data)
+	tx := database.NewTx(database.NewAccount(req.From), req.Value, req.Repository, req.Commit, req.prevCommit, false)
 	err = node.AddPendingTX(tx, node.info)
+	if err != nil {
+		writeErrRes(w, err)
+		return
+	}
+
+	writeRes(w, TxAddRes{Success: true})
+}
+
+// worker chooses transaction to mine
+func txMineHandler(w http.ResponseWriter, r *http.Request, node *Node) {
+	req := TxReq{}
+	err := readReq(r, &req)
+	if err != nil {
+		writeErrRes(w, err)
+		return
+	}
+
+	req.Mining = true
+	err = node.syncMiningTX(tx)
 	if err != nil {
 		writeErrRes(w, err)
 		return
@@ -68,7 +97,8 @@ func txAddHandler(w http.ResponseWriter, r *http.Request, node *Node) {
 func statusHandler(w http.ResponseWriter, r *http.Request, node *Node) {
 	res := StatusRes{
 		Hash:       node.state.LatestBlockHash(),
-		Number:     node.state.LatestBlock().Header.Number,
+		BlockIndex: node.state.LatestBlock().Header.Index,
+		TxIndex: 	node.state.LatestTx().Index,
 		KnownPeers: node.knownPeers,
 		PendingTXs: node.getPendingTXsAsArray(),
 	}
