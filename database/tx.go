1d66679bb03b747c18a8148fe4d2025877d60819
Robert Bublik <pulzlulz@gmail.com>
2021-05-10 19:03:49 +0200

    add occupied bool to Tx

--- database/tx.go
+++ database/tx.go
@@ -19,10 +19,11 @@ type Tx struct {
 	Commit 		20[byte] 	`json:"commit"`
 	prevCommit 	20[byte] 	`json:"prevCommit"`
 	Time  		uint64  	`json:"time"`
+	Occupied	bool		`json:"occupied"`
 }
 
-func NewTx(from Account, value uint, repository string, commit 20[byte], prevCommit 20[byte]) Tx {
-	return Tx{from, to, value, repository, commit, prevCommit, uint64(time.Now().Unix())}
+func NewTx(from Account, value uint, repository string, commit 20[byte], prevCommit 20[byte], occupied bool) Tx {
+	return Tx{from, to, value, repository, commit, prevCommit, uint64(time.Now().Unix()) occupied}
 }
 
 func (t Tx) IsReward() bool {
